b2e3c605534c7104b1febf04eb3eadeec1feb316
Emersonfeernandes <80550746+Emersonfeernandes@users.noreply.github.com>
2026-02-18 22:24:50 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,12 +15,43 @@ float calcularDensidade(int populacao, float area) {
     return (float) populacao / area;
 }
 
+// Função para exibir comparação entre as cartas
+void exibirComparacao(float C1densidade, float C2densidade, float C1area, float C1PIB, 
+    float C2area, float C2PIB, float C1perCapita, float C2perCapita,
+    int C1populacao, int C2populacao, int C1pontosTuristico, int C2pontosTuristico)
+    {
+    
+    float somaC1 = C1populacao + C1area + C1PIB +
+               C1pontosTuristico + C1densidade + C1perCapita;
+
+    float somaC2 = C2populacao + C2area + C2PIB +
+                C2pontosTuristico + C2densidade + C2perCapita;
+
+    float resultadoSoma = (somaC1 > somaC2) ? somaC1 : somaC2;
+
+    printf("\nComparação de Cartas:\n");
+    printf("População: Carta %d venceu\n",
+           (C1populacao > C2populacao) ? 1 : 2);
+    printf("Área: Carta %d venceu\n",
+           (C1area > C2area) ? 1 : 2);
+    printf("PIB: Carta %d venceu\n",
+           (C1PIB > C2PIB) ? 1 : 2);
+    printf("Pontos Turísticos: Carta %d venceu\n",
+           (C1pontosTuristico > C2pontosTuristico) ? 1 : 2);
+    printf("Densidade Populacional: Carta %d venceu\n",
+           (C1densidade < C2densidade) ? 1 : 2);
+    printf("PIB per Capita: Carta %d venceu\n",
+           (C1perCapita > C2perCapita) ? 1 : 2);
+    printf("Super Poder: %.2f, carta %d venceu\n", resultadoSoma, 
+           (somaC1 > somaC2) ? 1 : 2);
+}
+
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
   char C1estado[4], C1cidade[20], C1letra, C2estado[4], C2cidade[20], C2letra;
-  int C1populacao, C1pontosTuristico, C2populacao, C2pontosTuristico;
-  float C1area, C1PIB, C2area, C2PIB; 
-
+  unsigned int C1populacao, C1pontosTuristico, C2populacao, C2pontosTuristico;
+  float C1area, C1PIB, C2area, C2PIB, C1perCapita, C2perCapita, C1densidade, C2densidade; 
+  
   // Área para entrada de dados
   // CARTA 1
   printf("Digite a letra do Estado de 'A' a 'H' da carta 1: ");
@@ -70,6 +101,15 @@ int main() {
   printf("Qual é o PIB da cidade da carta 2? ");
   scanf("%f", &C2PIB);
 
+  // informações de Renda per capita e densidade da carta 1
+  C1densidade = calcularDensidade(C1populacao, C1area);
+  C1perCapita = calcularPIBperCapita(C1PIB, C1populacao);
+
+  // informações de Renda per capita e densidade da carta 2
+  C2densidade = calcularDensidade(C2populacao, C2area);
+  C2perCapita = calcularPIBperCapita(C2PIB, C2populacao);
+
+
   // Área para exibição dos dados da cidade
   // PRINTs DA CARTA 1
   printf("\nCarta 1\n");
@@ -80,8 +120,8 @@ int main() {
   printf("Área: %.2f km²\n", C1area);
   printf("PIB: %.2f bilhões de reais\n", C1PIB);
   printf("Número de Pontos Turísticos: %d\n", C1pontosTuristico);
-  printf("Densidade Populacional: %.2f hab/km²\n", calcularDensidade(C1populacao, C1area));
-  printf("PIB per Capita: %.2f reais\n", calcularPIBperCapita(C1PIB, C1populacao));
+  printf("Densidade Populacional: %.2f hab/km²\n", C1densidade);
+  printf("PIB per Capita: %.2f reais\n", C1perCapita);
 
   // PRINTs DA CARTA 2
   printf("\nCarta 2\n");
@@ -92,8 +132,12 @@ int main() {
   printf("Área: %.2f km²\n", C2area);
   printf("PIB: %.2f bilhões de reais\n", C2PIB);
   printf("Número de Pontos Turísticos: %d\n", C2pontosTuristico);
-  printf("Densidade Populacional: %.2f hab/km²\n", calcularDensidade(C2populacao, C2area));
-  printf("PIB per Capita: %.2f reais\n", calcularPIBperCapita(C2PIB, C2populacao));
+  printf("Densidade Populacional: %.2f hab/km²\n", C2densidade);
+  printf("PIB per Capita: %.2f reais\n", C2perCapita);
+
+  // chamando função para exibir comparação entre as cartas
+  exibirComparacao(C1densidade, C2densidade, C1area, C1PIB, C2area, C2PIB,
+    C1perCapita, C2perCapita, C1populacao, C2populacao, C1pontosTuristico, C2pontosTuristico);
 
   return 0;
 } 
